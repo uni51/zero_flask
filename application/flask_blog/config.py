30329e4c5ed2725cba8ab39ac721fc16a2a4dc3c
uni51 <uni51@users.noreply.github.com>
2019-05-20 17:35:54 +0900

    ssection19 finished

---
+++ application/flask_blog/config.py
@@ -0,0 +1,7 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+DEBUG = True
+# 本番ではセキュリティを高めるため、より長いランダムな値を設定する
+SECRET_KEY = 'secret key'
+USERNAME = 'john'
+PASSWORD = 'due123'
---
+++ application/flask_blog/views.py
@@ -0,0 +1,30 @@
+from flask import request, redirect, url_for, render_template, flash, session
+from flask_blog import app
+
+@app.route('/')
+def show_entries():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('entries/index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            flash('ユーザー名が異なります')
+        elif request.form['password'] != app.config['PASSWORD']:
+            flash('パスワードが異なります')
+        else:
+            session['logged_in'] = True
+            flash('ログインしました')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    # popはsessionオブジェクトから'logged_in'の項目を削除し、その値を返すという処理をする。
+    # もしその項目がない場合は第二引数でわたされたものを返す(この場合はNone)
+    session.pop('logged_in', None)
+    flash('ログアウトしました')
+    return redirect(url_for('show_entries'))
